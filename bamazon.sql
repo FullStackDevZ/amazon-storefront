5d9c8c8f70905f26d275a79bbcc62f7536bf016b
Ali Sarkeshik <alisarkeshik@gmail.com>
2019-05-17 23:13:57 -0700

    created sql file, won't output

---
+++ bamazon.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS amazon;
+
+CREATE DATABASE amazon;
+
+USE amazon;
+
+CREATE TABLE products (
+ id INTEGER NOT NULL AUTO_INCREMENT,
+  item_id INTEGER NULL,
+  product_name VARCHAR (100) NULL,
+  department_name VARCHAR (100) NULL,
+  price DECIMAL (10, 2) NULL,
+  stock_quantity INTEGER NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (334568, "Maple Syrup", "Food", 2.00, 200);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (331789, "Trapper Keeper", "Stationary", 3.99, 197);
